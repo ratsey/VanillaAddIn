7555a2d2e43b1854a36b1c622bc436ccf3a95571
Jens Ratsey-Woodroffe <jens@ratsey.com>
2018-01-20 09:04:16 -0500

    Add icon to setup form

--- VanillaAddIn/Setup.Designer.cs
+++ VanillaAddIn/Setup.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSetup));
             this.txtMLOExe = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
@@ -176,6 +177,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnOK);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmSetup";
